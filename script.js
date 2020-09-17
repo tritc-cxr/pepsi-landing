2d6f0f4de2c5b88b9ada0148189270a8181e1c39
Tri Truong <tri.truong@s3corp.com.vn>
2020-09-17 18:51:55 +0700

    done

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSlider(whatever) {
+    document.querySelector('.pepsi').src=whatever;
+}
+
+function changeBgColor(color) {
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
